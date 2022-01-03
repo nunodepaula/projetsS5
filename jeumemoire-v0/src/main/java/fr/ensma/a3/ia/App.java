62ae9c949295a79b3fb9c907651ed1f0f0ec4423
alvaresn <alvaresn@ensma.fr>
2022-01-03 16:01:30 +0100

    Projet Maven

---
+++ jeumemoire-v0/src/main/java/fr/ensma/a3/ia/App.java
@@ -0,0 +1,13 @@
+package fr.ensma.a3.ia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
