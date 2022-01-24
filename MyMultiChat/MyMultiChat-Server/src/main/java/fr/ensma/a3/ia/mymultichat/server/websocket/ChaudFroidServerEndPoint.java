e5b1ee28e73375af51446fc0407c1142de92af89
Nuno de Paula <nunodepaula@gmail.com>
2022-01-25 00:05:06 +0100

    test12

--- MyMultiChat/MyMultiChat-Server/src/main/java/fr/ensma/a3/ia/mymultichat/server/websocket/ChaudFroidServerEndPoint.java
+++ MyMultiChat/MyMultiChat-Server/src/main/java/fr/ensma/a3/ia/mymultichat/server/websocket/ChaudFroidServerEndPoint.java
@@ -91,7 +91,8 @@ public class ChaudFroidServerEndPoint {
 			Integer contenuValeur = Integer.valueOf(contenu);
 			
 			if (contenuValeur == valeur) {
-				messServer.setLeContenu(pseudo + " a envoyé : " + contenu + " - Trouvé !!");
+				messServer.setLeContenu(pseudo + " a envoyé : " + contenu + " - Trouvé !!" + 
+						" /n Canal doconnecté, tapez entrer pour continuer");
 			} else if (contenuValeur > valeur) {
 				messServer.setLeContenu(pseudo + " a envoyé : " + contenu + " - Trop grand");
 			} else {
@@ -109,20 +110,18 @@ public class ChaudFroidServerEndPoint {
 					e.printStackTrace();
 				}
 			}
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			if (contenuValeur == valeur) {
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				for (Session client : clients) {
 					if (!sess.getId().equals(client.getId())) {
 						try {
 							client.close();
 						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
 						}
 					}
 				try {
