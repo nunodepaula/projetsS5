53c5e5722d0c2a71703addb3b5069ef6ffe08026
alvaresn <alvaresn@ensma.fr>
2022-01-31 17:05:15 +0100

    fonction pour FragLogout (Android)

--- Workspace-Android/TestApp/app/src/main/java/fr/ensma/a3/ia/testapp/fragments/logout/FragLogout.java
+++ Workspace-Android/TestApp/app/src/main/java/fr/ensma/a3/ia/testapp/fragments/logout/FragLogout.java
@@ -1,4 +1,34 @@
 package fr.ensma.a3.ia.testapp.fragments.logout;
 
-public class FragLogout {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.core.view.GestureDetectorCompat;
+
+public class FragLogout extends Fragment implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+    GestureDetectorCompat mDetector;
+    ImageView imageBye;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        imageBye = (ImageView) getActivity().findViewById(R.id.imageLog);
+        mDetector = new GestureDetectorCompat(getActivity(), this);
+        mDetector.setOnDoubleTapListener(this);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                mDetector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
+    }
+
+    public void addObserver(){}
+
 }
--- Workspace-Android/TestApp/app/src/main/java/fr/ensma/a3/ia/testapp/fragments/logout/IFragLogout.java
+++ Workspace-Android/TestApp/app/src/main/java/fr/ensma/a3/ia/testapp/fragments/logout/IFragLogout.java
@@ -3,5 +3,7 @@ package fr.ensma.a3.ia.testapp.fragments.logout;
 import android.app.Fragment;
 import android.view.GestureDetector;
 
-public interface IFragLogout extends Fragment implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+public interface IFragLogout  {
+
+
 }
