52bdb32781a35c577cf7d91167952215030ef115
alvaresn <alvaresn@ensma.fr>
2022-01-31 16:58:14 +0100

    implements pour IFragLogout (Android)

--- Workspace-Android/TestApp/app/src/main/java/fr/ensma/a3/ia/testapp/fragments/logout/IFragLogout.java
+++ Workspace-Android/TestApp/app/src/main/java/fr/ensma/a3/ia/testapp/fragments/logout/IFragLogout.java
@@ -1,4 +1,7 @@
 package fr.ensma.a3.ia.testapp.fragments.logout;
 
-public interface IFragLogout {
+import android.app.Fragment;
+import android.view.GestureDetector;
+
+public interface IFragLogout extends Fragment implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
 }
