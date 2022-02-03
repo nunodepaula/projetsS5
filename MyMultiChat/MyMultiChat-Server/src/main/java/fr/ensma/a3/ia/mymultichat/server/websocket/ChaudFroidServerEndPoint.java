f351d26dd354ff37cf4272efd77b29308d564c25
Nuno de Paula <nunodepaula@gmail.com>
2022-02-03 17:13:03 +0100

    chaud froid

--- MyMultiChat/MyMultiChat-Server/src/main/java/fr/ensma/a3/ia/mymultichat/server/websocket/ChaudFroidServerEndPoint.java
+++ MyMultiChat/MyMultiChat-Server/src/main/java/fr/ensma/a3/ia/mymultichat/server/websocket/ChaudFroidServerEndPoint.java
@@ -27,7 +27,6 @@ import fr.ensma.a3.ia.mymultichat.api.messages.client.ClientMessage;
 import fr.ensma.a3.ia.mymultichat.api.messages.client.ClientMessageDecoder;
 import fr.ensma.a3.ia.mymultichat.api.messages.client.ClientMessageEncoder;
 
-
 /**
  * @author nunod
  *
@@ -38,67 +37,67 @@ public class ChaudFroidServerEndPoint {
 	static Map<String, Set<Session>> dictionary = new HashMap<String, Set<Session>>();
 	static Map<String, Queue<String>> tour = new HashMap<String, Queue<String>>();
 	static Map<String, Integer> valeurs = new HashMap<String, Integer>();
-	
+
 	@OnOpen
-	public void onOpen(@PathParam("canalandpseudo") String canalandpseudo, Session sess, EndpointConfig endpointConfig) {
+	public void onOpen(@PathParam("canalandpseudo") String canalandpseudo, Session sess,
+			EndpointConfig endpointConfig) {
 		System.out.println("Nouveau joueur au chaud froid");
 		System.out.println(sess.getId() + " vient de se connecter au canal " + canalandpseudo);
 		String[] params = canalandpseudo.split(":");
 		sess.getUserProperties().put("pseudo", params[1]);
 		sess.getUserProperties().put("canalId", params[0]);
-		
+
 		String canalId = params[0];
-		
+
 		if (dictionary.get(canalId) != null) {
 			dictionary.get(canalId).add(sess);
-			
+
 			Queue<String> q = tour.get(canalId);
 			q.add(params[1]);
 			tour.put(canalId, q);
-		}
-		else {
+		} else {
 			Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
 			clients.add(sess);
 			dictionary.put(canalId, clients);
-			
+
 			Queue<String> q = new LinkedList<String>();
 			q.add(params[1]);
 			tour.put(canalId, q);
-			
+
 			Random rand = new Random();
 			Integer valeur = rand.nextInt(100);
 			valeurs.put(canalId, valeur);
 		}
 	}
 
-	//Réaction du serveur à la réception du serveur
+	// Réaction du serveur à la réception du serveur
 	@OnMessage
 	public void onMessage(ClientMessage mess, Session sess) {
 		String canalId = sess.getUserProperties().get("canalId").toString();
 		String pseudo = sess.getUserProperties().get("pseudo").toString();
-		
+
 		Set<Session> clients = dictionary.get(canalId);
 		Queue<String> queueTour = tour.get(canalId);
 		String pseudoTour = queueTour.peek();
 		Integer valeur = valeurs.get(canalId);
-		
+
 		ClientMessage messServer = new ClientMessage();
 		messServer.setLePseudo("LeServer");
 		if (pseudo == pseudoTour) {
 			// Logique du jeu
 			String contenu = mess.getLeContenu();
-			
+
 			Integer contenuValeur = Integer.valueOf(contenu);
-			
+
 			if (contenuValeur == valeur) {
-				messServer.setLeContenu(pseudo + " a envoyé : " + contenu + " - Trouvé !!" + 
-						" /n Canal doconnecté, tapez entrer pour continuer");
+				messServer.setLeContenu(pseudo + " a envoyé : " + contenu + " - Trouvé !!"
+						+ " /n Canal doconnecté, tapez entrer pour continuer");
 			} else if (contenuValeur > valeur) {
 				messServer.setLeContenu(pseudo + " a envoyé : " + contenu + " - Trop grand");
 			} else {
 				messServer.setLeContenu(pseudo + " a envoyé : " + contenu + " - Trop petit");
 			}
-			
+
 			for (Session client : clients) {
 				try {
 					client.getBasicRemote().sendObject(messServer);
@@ -124,10 +123,10 @@ public class ChaudFroidServerEndPoint {
 						} catch (IOException e) {
 						}
 					}
-				try {
-					sess.close();
-				} catch (IOException e) {
-				}
+					try {
+						sess.close();
+					} catch (IOException e) {
+					}
 				}
 			} else {
 				// Changer le tour
@@ -162,21 +161,21 @@ public class ChaudFroidServerEndPoint {
 			}
 		}
 	}
-	
+
 	@OnClose
 	public void onClose(Session sess) {
 		System.out.println(sess.getUserProperties().get("pseudo") + " vient de se déconnecter ...");
-		//clients.remove(sess);
-		
+		// clients.remove(sess);
+
 		String canalId = sess.getUserProperties().get("canalId").toString();
 		String pseudo = sess.getUserProperties().get("pseudo").toString();
 		dictionary.get(canalId).remove(sess);
 		Set<Session> clients = dictionary.get(canalId);
-		
+
 		Queue<String> queueTour = tour.get(canalId);
 		queueTour.remove(pseudo);
 		tour.put(canalId, queueTour);
-		
+
 		ClientMessage mess = new ClientMessage();
 		for (Session client : clients) {
 			mess.setLePseudo("LeServer");
@@ -191,16 +190,16 @@ public class ChaudFroidServerEndPoint {
 				e.printStackTrace();
 			}
 		}
-		
+
 		if (clients.isEmpty()) {
 			dictionary.remove(canalId);
 			tour.remove(canalId);
 			valeurs.remove(canalId);
 		}
 	}
-	
-	  @OnError
-	  public void onError(Session session, Throwable t) {
-	    t.printStackTrace();
-	  }
+
+	@OnError
+	public void onError(Session session, Throwable t) {
+		t.printStackTrace();
+	}
 }
