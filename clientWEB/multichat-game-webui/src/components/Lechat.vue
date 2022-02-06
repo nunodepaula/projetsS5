26826d10067abcf29b5b4f6518a7111ade9b21b9
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 <nunodepaula@gmail.com>
2022-02-06 17:09:52 +0100

    version finale le chat

--- clientWEB/multichat-game-webui/src/components/Lechat.vue
+++ clientWEB/multichat-game-webui/src/components/Lechat.vue
@@ -1,7 +1,38 @@
 <template>
     <div>
     <h1>Mon app</h1>
-    {{ info }}
+    <table id='liste-chat'>
+        <thread>
+            <tr>
+                <th>Canaux</th>
+            </tr>
+        </thread>
+        <tbody>
+            <tr v-for="row in this.leschat" :key="row">
+                <td>{{ row }}</td>
+            </tr>
+        </tbody>
+    </table>
+    <h4>Sélectionner un des cannaux ci-dessous</h4>
+    <input type=text list=choixCanal v-model="canalChoisi">
+    <datalist id="choixCanal">
+        <option v-for="row in this.leschat" :key="row"> {{ row }} </option>
+    </datalist>
+
+    <button @click="chatConnect">
+        Connecter à {{canalChoisi}}
+    </button>
+
+    <div>
+        <span><pre>{{chatAllMessage}}</pre></span>
+    </div>
+
+    <input v-model="reponse" style="width:300px"/>
+    <button @click="sendReponse">Répondre</button>
+    <p>
+        <button @click="chatBye">Bye Bye</button>
+    </p>
+
     </div>
 </template>
 
@@ -11,7 +42,9 @@ export default {
   data() {
     return {
     ws : null,
-    serverUrl: "ws://localhost:8085/ws/multichat/1:Mikky",
+    canalChoisi:"",
+    canalId:"",
+    serverUrl: "ws://localhost:8080/ws/multichat/1:Mikky",
       leschat: {
         canalId: "",
         canalName: "",
@@ -32,7 +65,7 @@ export default {
   },
   mounted() {
     console.log("Appel service Rest ...")
-    axios.get("http://localhost:8085/services/multichat/1").then((response) => {
+    axios.get("http://localhost:8080/services/canaux-disponibles").then((response) => {
       this.leschat = response.data;
       console.log(this.leschat)
     });
@@ -56,10 +89,16 @@ export default {
     },
     chatConnect() {     
       console.log("Starting connection to WebSocket Server");
+      this.chatAllMessage = ""
+      this.canalId = this.canalChoisi[0];
+      this.chatReponse.canalId = this.canalId;
+      this.chatMessage.canalId = this.canalId;
+      this.serverUrl = "ws://localhost:8080/ws/multichat/"+this.canalId+':'+ this.$store.getters.getLeUser
       this.ws = new WebSocket( this.serverUrl );
       this.ws.onopen = function (event) {
         console.log(event);
         console.log("On est connecté !!!");
+        console.log(this.canalId);
       };
       this.ws.addEventListener('message', (event) => { this.handleNewMessage(event) });
     }
